acd4fe2769ff3b959a4e464f578398f55cc3e4c5
Joona Viertola <git@joonaviertola.com>
2022-10-23 12:59:29 +0300

    Add Toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { MouseEventHandler, useState } from "react";
 import "./styles/variables.css";
 import "./styles/reset.css";
 import "./styles/typography.css";
@@ -7,19 +7,21 @@ import AppContainer from "./components/AppContainer";
 import InputBlock from "./components/InputBlock";
 import ClassList from "./components/ClassList";
 import Button from "./components/Button";
+import Toggle from "./components/Toggle";
 
 const horizontalContainerStyle = {
-  display: 'flex',
-  flexDirection: 'row' as 'row',
-  width: '100%',
+  display: "flex",
+  flexDirection: "row" as "row",
+  width: "100%",
   marginTop: 20,
-  // justifyContent: 'space-between'
-}
+};
 
 function App() {
-  const [height, setHeight] = useState('');
-  const [weight, setWeight] = useState('');
-  const [bmi, setBmi] = useState('');
+  const [unitSystem, setUnitSystem] = useState("metric");
+
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [bmi, setBmi] = useState("");
 
   const isEmpty = (value: string) => {
     return !parseFloat(value);
@@ -29,22 +31,22 @@ function App() {
     return !Number.isNaN(parseFloat(value)) && parseFloat(value) !== 0;
   };
 
-  const heightOnchangeHandler = (event: React.FormEvent<HTMLInputElement> ) => {
-    setHeight(event.currentTarget.value)
-  }
+  const heightOnchangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setHeight(event.currentTarget.value);
+  };
 
-  const weightOnchangeHandler = (event: React.FormEvent<HTMLInputElement> ) => {
-    setWeight(event.currentTarget.value)
-  }
+  const weightOnchangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setWeight(event.currentTarget.value);
+  };
 
-  const bmiOnchangeHandler = (event: React.FormEvent<HTMLInputElement> ) => {
-    setBmi(event.currentTarget.value)
-  }
+  const bmiOnchangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setBmi(event.currentTarget.value);
+  };
 
   const buttonOnClickHandler = () => {
     // Height
     if (isDefined(bmi) && isDefined(weight) && isEmpty(height)) {
-      const result = (Math.sqrt((Number(weight) / Number(bmi))) * 100).toFixed(0);
+      const result = (Math.sqrt(Number(weight) / Number(bmi)) * 100).toFixed(0);
       setHeight(result);
     }
 
@@ -56,32 +58,50 @@ function App() {
 
     // BMI
     if (isDefined(height) && isDefined(weight) && isEmpty(bmi)) {
-      const result = ((Number(weight) / Number(height) ** 2) * 10000).toFixed(0);
+      const result = ((Number(weight) / Number(height) ** 2) * 10000).toFixed(
+        0
+      );
       setBmi(result);
     }
-  }
+  };
+
+  const unitSystemOptions = [
+    { value: "metric", name: "METRIC" },
+    { value: "imperial", name: "IMPERIAL" },
+  ];
+
+  const unitSystemHandler: MouseEventHandler<HTMLButtonElement> = (event) => {
+    const target = event.target as HTMLButtonElement;
+    setUnitSystem(target.value);
+  };
 
   return (
     <AppContainer>
-      <InputBlock name="HEIGHT" value={height} handler={heightOnchangeHandler} />
+      <Toggle options={unitSystemOptions} handler={unitSystemHandler} selected={unitSystem} />
+      <InputBlock
+        name="HEIGHT"
+        value={height}
+        handler={heightOnchangeHandler}
+        style={{ marginTop: "2rem" }}
+      />
       <div style={horizontalContainerStyle}>
         <InputBlock
           name="WEIGHT"
           value={weight}
           handler={weightOnchangeHandler}
-          style={{ marginRight: '10px', flexGrow: 1 }}
+          style={{ marginRight: "10px", flexGrow: 1 }}
         />
         <InputBlock
           name="BMI"
           value={bmi}
           handler={bmiOnchangeHandler}
-          style={{ marginLeft: '10px', flexGrow: 1 }}
+          style={{ marginLeft: "10px", flexGrow: 1 }}
         />
       </div>
 
-      <ClassList style={{ marginTop: '2rem' }} />
+      <ClassList style={{ marginTop: "2rem" }} />
 
-      <Button handler={buttonOnClickHandler} style={{ marginTop: '2rem' }} />
+      <Button handler={buttonOnClickHandler} style={{ marginTop: "2rem" }} />
     </AppContainer>
   );
 }
---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,28 @@
+import React, { MouseEventHandler } from "react";
+import styles from "./Toggle.module.css";
+
+type Option = {
+  name: string;
+  value: string;
+};
+
+type ToggleProps = {
+  handler: MouseEventHandler<HTMLButtonElement>;
+  style?: React.CSSProperties;
+  options: Option[];
+  selected: string;
+};
+
+const Toggle = ({ handler, style, options = [], selected }: ToggleProps) => {
+  return (
+    <div className={styles.container} style={style}>
+      {options.map((item: Option) => {
+        const classes = [styles.option];
+        if (item.value === selected) classes.push(styles.selectedOption);
+        return <button className={classes.join(' ')} value={item.value} onClick={handler}>{item.name}</button>;
+      })}
+    </div>
+  );
+};
+
+export default Toggle;
